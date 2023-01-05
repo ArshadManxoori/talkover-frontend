2c73ad342bc92e00cd4a6440dfefdf5df879ee63
ArshadManxoori <mdarshadmansoori@gmail.com>
2023-01-05 14:11:38 +0530

    socket2

--- src/socket/index.js
+++ src/socket/index.js
@@ -7,7 +7,7 @@ const socketInit = () => {
         timeout: 10000,
         transports: ['websocket'],
     };
-    return io('http://localhost:5500', options);
+    return io('https://talkover-api.onrender.com', options);
 };
 
 export default socketInit;
